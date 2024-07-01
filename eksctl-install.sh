92006404665285fdd466414086e901028ab4386a
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-07-01 19:44:46 +0530

    install scripting for docker eks,kube ctl

--- eksctl-install.sh
+++ eksctl-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
+touch /tmp/eksctl.log
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp >> /tmp/eksctl.log
 sudo mv /tmp/eksctl /usr/local/bin
-touch /tmp/eksctl.log
 eksctl version >> /tmp/eksctl.log
