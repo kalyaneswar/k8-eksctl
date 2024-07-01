1c6b4440283396c6e8cf52c0c4bc74a2d4a76d68
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-07-01 19:35:05 +0530

    install scripting for docker eks,kube ctl

--- disk-resize.sh
+++ disk-resize.sh
@@ -2,7 +2,8 @@
 
 touch /tmp/disk-resize.log 
 lsblk >> /tmp/disk-resize.log 
-sudo growpart /dev/nvme0n1 4 >> /tmp/disk-resize.log 
+# sudo growpart /dev/nvme0n1 4 >> /tmp/disk-resize.log #for t3.micro
+sudo growpart /dev/xvda 4 >> /tmp/disk-resize.log  # for t2.micro
 sudo lvextend -l +50%FREE /dev/RootVG/rootVol >> /tmp/disk-resize.log 
 sudo lvextend -l +50%FREE /dev/RootVG/varVol >> /tmp/disk-resize.log 
 sudo xfs_growfs / >> /tmp/disk-resize.log 
